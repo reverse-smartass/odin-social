20d794f3c7b9de2573491cdbc52eefb1e99671d4
reverse-smartass <nguyenminhdore@gmail.com>
2026-04-15 15:54:48 -0400

    commit

--- api/postsRoute.js
+++ api/postsRoute.js
@@ -2,7 +2,6 @@ import { Router } from "express";
 import prisma from "../lib/prisma.ts";
 import { body, validationResult } from "express-validator";
 import passport from "passport";
-import { disconnect } from "node:cluster";
 const postRouter = Router();
 
 const validatePost = [
@@ -77,12 +76,6 @@ postRouter.post("/newpost", passport.authenticate("jwt", { session: false }), va
       data: {
         title: title,
         content: text_content,
-        Likes: {
-          connect: []
-        },
-        comments: {
-          connect: []
-        },
         author : {
           connect: {id : req.user.id}
         }
@@ -161,12 +154,12 @@ postRouter.delete("/:postid/delete", passport.authenticate("jwt", { session: fal
   
 });
 
-postRouter.get("/", async (req, res) => {
+postRouter.get("/", passport.authenticate("jwt", { session: false }), async (req, res) => {
   const result = await prisma.post.findMany();
   res.json({ result });
 });
 
-postRouter.get("/:postid", async (req, res) => {
+postRouter.get("/:postid", passport.authenticate("jwt", { session: false }), async (req, res) => {
   const postId = req.params.postid;
 
   const result = await prisma.post.findUnique({
@@ -178,163 +171,53 @@ postRouter.get("/:postid", async (req, res) => {
   res.json(result);
 });
 
-postRouter.get("/:postid/comments", async (req, res) => {
+postRouter.get("/:postid/replies", async (req, res) => {
   const postId = req.params.postid;
 
-  const result = await prisma.comment.findMany({
+  const result = await prisma.post.findMany({
     where: {
-      postId: postId,
+      replyToId: postId,
     },
   });
 
   res.json(result);
 });
 
-const validateComment = [
-  body("comment")
-    .trim()
-    .notEmpty()
-    .withMessage("Comment can't be empty")
-    .escape(),
-];
-
-postRouter.post("/:postid/newcomment", passport.authenticate("jwt", { session: false }), validateComment,
-async (req, res, next) => {
-  const postId = req.params.postid;
-
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.json({
-      errors: errors.array(),
-      previousData: req.body,
-    });
-  }
-
-  const {comment} = req.body;
-
-  try {
-
-    const savedComment = await prisma.comment.create({
-      data: {
-        content: comment,
-        author : {
-          connect: {id : req.user.id}
-        },
-        post : {
-          connect: {id : postId}
-        }
-      }
-    });
-    console.log("Comment saved:", comment);
-    res.status(201).json({
-      message: "comment saved",
-      comment: { savedComment },
-    });
-  } catch (err) {
-    return next(err);
-  }
-});
-
-postRouter.patch("/edit-comment/:commentid/", passport.authenticate("jwt", { session: false }), isOwner("comment", "commentid"), validateComment,
-async (req, res, next) => {
-  const commentId = req.params.commentid;
-
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.json({
-      errors: errors.array(),
-      previousData: req.body,
-    });
-  }
-
-  const {comment} = req.body;
-
-  try {
-    const savedComment = await prisma.comment.update({
-      where: {
-        id: commentId,
-      },
-      data: {
-        content: comment,
-      }
-    });
-    console.log("Comment saved:", comment);
-    res.status(200).json({
-      message: "comment saved",
-      comment: { savedComment },
-    });
-  } catch (err) {
-    return next(err);
-  }
-});
-
-postRouter.delete("/delete-comment/:commentid/", passport.authenticate("jwt", { session: false }), isOwner("comment", "commentid"), 
-async (req, res, next) => {
-  const commentId = req.params.commentid;
-
-  try {
-    const deletedComment = await prisma.comment.delete({
-      where: {
-        id: commentId,
-      },
-    });
-    console.log("Comment deleted:", deletedComment);
-    res.status(200).json({
-      message: "comment deleted",
-      comment: { deletedComment},
-    });
-  } catch (err) {
-    return next(err);
-  }
-});
-
-postRouter.patch("/toggle-like-comment/:postid/", passport.authenticate("jwt", { session: false }),
+postRouter.patch("/:postid/toggle-like/", passport.authenticate("jwt", { session: false }),
 async (req, res, next) => {
   const postId = req.params.postid;
   const userId = req.user.id;
 
   try {
     // 1. Check if the like already exists
-    const existingLike = await prisma.post.findUnique({
+
+    const post = await prisma.post.findUnique({
       where: {
-        likes: {
-          some: {
-            userId: userId,
-            postId: postId,
-          }
+        id: postId,
+      },
+      include: {
+        Likes: {where: {id: userId}},
+      }
+    });
+
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    const existingLike = post.Likes.length > 0;
+
+    await prisma.post.update({
+      where: {
+        id: postId,
+      },
+      data: {
+        Likes: {
+          [existingLike ? "disconnect" : "connect"]: { id: userId }
         }
       }
     });
 
-    if (existingLike) {
-      // 2. If it exists, UNLIKE (delete)
-      await prisma.post.update({
-        where: { 
-          postId: postId,
-        },
-        data: {
-          Likes: {
-            disconnect: {
-              id: userId
-            }
-          }
-        }
-      });
-      return res.json({ liked: false });
-    } else {
-      // 3. If it doesn't exist, LIKE (create)
-      await prisma.post.update({
-        where: {
-          postId: postId,
-        },
-        data: {Likes: {
-          connect: {
-            id: userId
-          }
-        }}
-      });
-      return res.json({ liked: true });
-    }
+    return res.json({ liked: !existingLike });
   } catch (err) {
     res.status(500).json({ error: "Failed to toggle like"  });
   }
